369891bd16a33e502c4da4a962cfee9108c6f3e9
Emanuel Alamilla <e.alamilla318@gmail.com>
2021-05-10 23:18:59 -0500

    registro usuarios encriptacion sin imagen

--- api/InventarioAPI/routes/api/usuarios.js
+++ api/InventarioAPI/routes/api/usuarios.js
@@ -1,12 +1,20 @@
 const { Usuario } = require('../../db');
 const router = require('express').Router();
 const bcrypt = require('bcryptjs')
+const {check, validationResult} =  require('express-validator');
 
+router.post('/register',[
+    check('correoUsuario','El formato de email es incorrecto').isEmail()
+],async(req,res)=>{
+    const errors =  validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errores: errors.array()});
+    }
 
-router.post('/register',async(req,res)=>{
-    req.body.passwordUsuario = bcrypt.hashSync(req.body.passwordUsuario, 10);
-    const usuario = Usuario.create(req.body);
-    res.json(user);
+    req.body.passwordUsuario = bcrypt.hashSync(req.body.passwordUsuario,10);
+
+    const usuario = await Usuario.create(req.body);
+    res.json(usuario);
 });
 
 
