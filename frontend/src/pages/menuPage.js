9cb1f3bd4d136c28cc7b3619d2b99856eaab11fe
Emanuel Alamilla <e.alamilla318@gmail.com>
2021-05-20 01:10:13 -0500

    menu update

--- frontend/src/pages/menuPage.js
+++ frontend/src/pages/menuPage.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import Navbar from '../components/sidebar/Navbar';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Navbar from "../components/sidebar/Navbar";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function Menu() {
   return (
     <>
-    <Navbar/>
-    <div>
-      
-      <div className='menu'>
-      <h1>Menu</h1>
-      
-    </div>
-
-    <div>
-    
-    </div>
-    </div>
+      <Navbar />
+      <div className="menu">
+        <h1>Menu</h1>
+      </div>
+      <div>
+        <Card>
+          <Card.Header as="h5">Featured</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 }
