7777400441028cf8930c4c19e827bd46bf42d7a2
FredyNaranjo19 <fredynaranjo199@hotmail.com>
2021-05-19 23:45:04 -0500

    responsive

--- frontend/src/pages/proveedoresPage.js
+++ frontend/src/pages/proveedoresPage.js
@@ -114,7 +114,7 @@ function Proveedores() {
           <Button variant="primary" onClick={OpenCloseModalInsert}>
             Nuevo Proveedor
           </Button>
-          <Table striped bordered hover variant ="dark">
+          <Table responsive="sm" striped bordered hover variant ="dark">
             <thead>
               <tr>
                 <th>Id</th>
